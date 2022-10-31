dc5cd9ca36a6bdfb491185b303fa2a980704d6e9
JeongUpGi <dkdfl1201@gmail.com>
2022-10-31 18:03:24 +0900

    실습일지 10-1의 실습1 commit

--- textbook/src/index.js
+++ textbook/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
+import ContextApp from "./Chapter10/ContextApp";
+import ContextAppHook from "./Chapter10/ContextAppHook";
+import PageColor from "./Chapter10/PageColor";
+
 import App from "./Client/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<ContextApp />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
